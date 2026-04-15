aa426587ce5adb399806dfbd0da0c5882e7e6788
Tik-tak753 <80722076+Tik-tak753@users.noreply.github.com>
2026-04-15 18:36:50 +0300

    Optimize video inference throughput in AppController

--- controller/appcontroller.h
+++ controller/appcontroller.h
@@ -1,11 +1,15 @@
 #ifndef APPCONTROLLER_H
 #define APPCONTROLLER_H
 
+#include "detection/detection.h"
+
 #include <QImage>
 #include <QString>
 
+#include <chrono>
 #include <memory>
 
+#include <opencv2/core.hpp>
 #include <opencv2/videoio.hpp>
 
 class IDetector;
@@ -26,6 +30,12 @@ public:
     bool hasOpenVideo() const;
 
 private:
+    cv::Mat createVideoInferenceFrame(const cv::Mat &frame) const;
+    DetectionList remapDetectionsToDisplayFrame(const DetectionList &detections,
+                                                const cv::Size &inferenceSize,
+                                                const cv::Size &displaySize) const;
+    QString videoInferenceModeText() const;
+
     void activateStubDetector(const QString &reason);
     void activateYoloDetector(std::unique_ptr<YoloDetector> yoloDetector, const QString &modelPath);
     QString detectorStatusText() const;
@@ -36,6 +46,16 @@ private:
     QString detectorStatusDetail_;
     QString loadedVideoPath_;
     cv::VideoCapture videoCapture_;
+
+    bool reduceVideoInferenceResolution_ = true;
+    cv::Size videoInferenceResolution_ = cv::Size(640, 360);
+    int videoInferenceFrameSkip_ = 1;
+    int videoFrameIndex_ = 0;
+    DetectionList cachedVideoDetections_;
+
+    bool hasLastFrameTimestamp_ = false;
+    std::chrono::steady_clock::time_point lastFrameTimestamp_;
+    double smoothedFps_ = 0.0;
 };
 
 #endif // APPCONTROLLER_H
